a2722c7d16c21c4ba40b91709eb304fa5de61c00
Nishant Kohale <nishantkohale4@gmail.com>
2024-11-25 21:50:36 +0530

    Nishant :- Added users data in json format

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const express = require('express')
+const app = express()
+const PORT = 3005
+const users = require('./MOCK_DATA.json')
+
+app.get('/api/users', (req,res)=>{
+    return res.json(users)
+})
+
+app.get('/users/:id', (req,res)=>{
+    const id = Number(req.params.id)
+    const user = users.find(user => user.id == id)
+    return (res.json(user))
+})
+
+app.post('/users', (req,res)>{
+    
+})
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
