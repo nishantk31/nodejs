ad246a73e014c7b9963db9d3c28d59003b3d1f72
Nishant Kohale <nishantkohale4@gmail.com>
2024-11-30 13:16:39 +0530

    Nishant :- Added Code to get RESTful API

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 3005
 const users = require('./MOCK_DATA.json')
-
+const fs = require('fs')
+app.use(express.json())
 app.get('/api/users', (req,res)=>{
     return res.json(users)
 })
@@ -13,7 +14,21 @@ app.get('/users/:id', (req,res)=>{
     return (res.json(user))
 })
 
-app.post('/users', (req,res)>{
+app.post('/users', (req,res)=>{
+
+    const body = req.body
+    users.push({
+        id: users.length +1,
+        first_name:body.first_name,
+        last_name:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        job_title:body.job_title
+    })
+    
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
+        return res.json(users)
+    })
     
 })
 app.listen(PORT, ()=>{
